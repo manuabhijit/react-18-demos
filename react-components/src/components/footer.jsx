0b9ad1a251b6a22afac2540945cf7c11814d8462
Abhijit <abhijit_srivastava>
2023-01-05 03:10:50 +0530

    [Added] simple header footer

--- react-components/src/components/footer.jsx
+++ react-components/src/components/footer.jsx
@@ -1,10 +1,42 @@
+import React from 'react';
+
 function Footer() {
   return (
-    <div>
-      footer
-      
-    </div>
+    <footer style={styles.footer}>
+      <p style={styles.copyright}>Copyright 2021</p>
+      <div style={styles.socialMedia}>
+        <a href="#" style={styles.socialMediaLink}>
+          Twitter
+        </a>
+        <a href="#" style={styles.socialMediaLink}>
+          Facebook
+        </a>
+        <a href="#" style={styles.socialMediaLink}>
+          Instagram
+        </a>
+      </div>
+    </footer>
   );
 }
+
+const styles = {
+  footer: {
+    backgroundColor: '#333',
+    color: '#fff',
+    padding: '20px',
+  },
+  copyright: {
+    margin: 0,
+  },
+  socialMedia: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginTop: '20px',
+  },
+  socialMediaLink: {
+    color: '#fff',
+    textDecoration: 'none',
+  },
+};
 
 export default Footer;
--- react-components/src/components/header.jsx
+++ react-components/src/components/header.jsx
@@ -1,10 +1,53 @@
+import React from 'react';
+
 function Header() {
   return (
-    <div>
-      Header
-      
-    </div>
+    <header style={styles.header}>
+      <img src="/logo.png" alt="Logo" style={styles.logo} />
+      <nav style={styles.nav}>
+        <ul style={styles.navList}>
+          <li style={styles.navItem}>
+            <a href="#" style={styles.navLink}>Home</a>
+          </li>
+          <li style={styles.navItem}>
+            <a href="#" style={styles.navLink}>About</a>
+          </li>
+          <li style={styles.navItem}>
+            <a href="#" style={styles.navLink}>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
+
+const styles = {
+  header: {
+    backgroundColor: '#333',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '20px',
+  },
+  logo: {
+    height: '40px',
+  },
+  nav: {
+    display: 'flex',
+  },
+  navList: {
+    listStyle: 'none',
+    margin: 0,
+    padding: 0,
+    display: 'flex',
+  },
+  navItem: {
+    marginRight: '20px',
+  },
+  navLink: {
+    color: '#fff',
+    textDecoration: 'none',
+  },
+};
 
 export default Header;
